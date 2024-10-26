965ea5dc5f5607553a61a5904ed5e469cc6a12bd
Nhut Nguyen <nhutb2205896@student.ctu.edu.vn>
2024-10-26 15:53:14 +0700

    modify sum 4

--- sum.py
+++ sum.py
@@ -1,6 +1,7 @@
 num1 = float(input("Input the first number "))
 num2 = float(input("Input the second number "))
 num3 = float(input("Input the second number "))
-sum = num1 + num2 + num3
+num4 = float(input("Input the second number "))
+sum = num1 + num2 + num3 + num4
 
 print("Sum is: ", sum)
