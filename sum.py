670d83d9ffad0e89a934d6f8fd7ca698e3610449
Nhut Nguyen <nhutb2205896@student.ctu.edu.vn>
2024-10-26 15:40:47 +0700

    sum 3 number

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
 num1 = float(input("Input the first number "))
 num2 = float(input("Input the second number "))
-
-sum = num1 + num2
+num3 = float(input("Input the second number "))
+sum = num1 + num2 + num3
 
 print("Sum is: ", sum)
